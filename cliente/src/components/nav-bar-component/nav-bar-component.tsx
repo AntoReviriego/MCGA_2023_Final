dc7a1c00b6fc97264190aca890fca2a658ea3a16
AntoReviriego <65468918+AntoReviriego@users.noreply.github.com>
2023-12-20 09:47:45 -0300

    Modulo navbar. Se remplazan los href por link de react-router-dom para que no se recarge la pagina sino que se renderen los componentes

--- cliente/src/components/nav-bar-component/nav-bar-component.tsx
+++ cliente/src/components/nav-bar-component/nav-bar-component.tsx
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { auth } from "../../firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useUser } from "../../provider/user.context.provider";
 import { UserContextType } from "../../provider/type";
 const NavBar = () => {
@@ -34,23 +34,23 @@ const NavBar = () => {
         <>
             <Navbar expand="lg" bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand  className="text-white" href="/">Universidad Ficticia</Navbar.Brand>
+                    <Navbar.Brand  className="text-white"  as={Link} to="/">Universidad Ficticia</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav className="me-auto">
-                            <Nav.Link  className="text-white" href="/noticias">Noticias</Nav.Link>
+                            <Nav.Link  className="text-white"  as={Link} to="/noticias">Noticias</Nav.Link>
                             {isAuthenticated && (
                                 <>
-                                    <Nav.Link  className="text-white" href="/carrera">Carrera</Nav.Link> 
-                                    <Nav.Link  className="text-white" href="/noticia-form"><i className="fas fa-plus-circle"></i> Nueva Noticia</Nav.Link>
+                                    <Nav.Link  className="text-white" as={Link} to="/carrera"  >Carrera</Nav.Link> 
+                                    <Nav.Link  className="text-white" as={Link} to="/noticia-form"><i className="fas fa-plus-circle"></i> Nueva Noticia</Nav.Link>
                                 </>
                             )}
                         </Nav>
                         <Nav>
                         {!isAuthenticated && (
                             <>
-                                <Nav.Link className="text-white" href="/login">Iniciar sesión</Nav.Link>
-                                <Nav.Link className="text-white" href="/registro">Registrarse</Nav.Link>
+                                <Nav.Link className="text-white" as={Link} to="/login">Iniciar sesión</Nav.Link>
+                                <Nav.Link className="text-white" as={Link} to="/registro">Registrarse</Nav.Link>
                             </>
                         )}
                         {isAuthenticated && (
