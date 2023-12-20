02b63a52350eb17ac780149721d55ce1399b4ef5
valencontrera <valentina.contrera14@gmail.com>
2023-12-20 11:18:59 -0300

    Se agrego logica para el momento de guardar archivos o imagenes, si la carpeta uploads no existe se crea antes de guardar

--- backend/services/subidaArchivo/subirArchivo.js
+++ backend/services/subidaArchivo/subirArchivo.js
@@ -1,15 +1,19 @@
 const multer = require('multer');
+const fs = require('fs'); // Módulo FileSystem de Node.js
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Ruta donde se guardarán los archivos subidos
+    const folder = 'uploads/';
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder, { recursive: true });    // Verificar si la carpeta existe, si no, la crea
+    }
+    cb(null, folder); // Ruta donde se guardarán los archivos subidos//   
   },
   filename: function (req, file, cb) {
     const { carrera, resolucion } = req.body;
     let nombreArchivo = "";
-    if(carrera != null && carrera != undefined){
+    if (carrera != null && carrera != undefined) {
       nombreArchivo = `${carrera.replace(/\s/g, '_')}_${resolucion.replace(/\s/g, '_')}_${Date.now()}_${file.originalname}`;
-    }
-    else{
+    } else {
       nombreArchivo = `_${Date.now()}_${file.originalname}`;
     }
     cb(null, nombreArchivo); // Nombre de archivo combinado único
